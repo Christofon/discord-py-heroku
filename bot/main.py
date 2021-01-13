e764616885cafe0d7b7ea882874dcf9b43ee1c9c
Christofon <kremsnerchristopher@gmail.com>
2021-01-13 04:15:42 +0100

    add dict to keep track of teams

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,14 @@
 import os
+import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
 bot = commands.Bot(command_prefix="!")
+
+load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
+
+teams = {}
 
 @bot.event
 async def on_ready():
@@ -12,22 +18,29 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send("pong")
 @bot.command()   
-async def neuesTurnier(ctx):
-    await ctx.send(f"Gib dem Turnier einen Namen:")
+async def openTeam(ctx):
+    await ctx.send(f"Wie soll das Team heißen?")
 
     def check(msg):
         return msg.author == ctx.author
 
     msg = await bot.wait_for("message", check=check)
-    tournament_name = msg.content
+    team_name = msg.content
 
-    await ctx.guild.create_voice_channel(tournament_name)
-    await ctx.send(msg.author) 
-    channel = ctx.utils.find(lambda x: x.name == tournament_name, ctx.guild.channels)
-#    creator = ctx.utils.find(lambda x: x.name == msg.author, ctx)
-  
-    await ctx.send(channel) 
-    await ctx.move_member(msg.author, channel)
+    await ctx.guild.create_voice_channel(team_name)
+    
+    channel = discord.utils.find(lambda x: x.name == team_name, ctx.guild.channels)
+    creator = msg.author
+
+    teams.update(team_name = [msg.author.name])
+    print(teams)
+
+    # user muss mit voice verbunden sein um moved werden zu können
+    await creator.move_to(channel)
+
+@bot.command()
+async def joinTeam(ctx):
+    pass
 
 if __name__ == "__main__":
     bot.run(TOKEN)
