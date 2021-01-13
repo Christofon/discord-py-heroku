f6584f594bac5b77b830176d20aa176bbfec3991
Christofon <kremsnerchristopher@gmail.com>
2021-01-13 03:03:07 +0100

    create channel and move member

--- bot/main.py
+++ bot/main.py
@@ -11,9 +11,19 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send("pong")
-    await ctx.guild.create_voice_channel('cool-channel')
    
+async def neuesTurnier(ctx):
+    await ctx.send(f"Gib dem Turnier einen Namen:")
 
+    def check(msg):
+        return msg.author == ctx.author
+
+    msg = await bot.wait_for("message", check=check)
+    tournament_name = msg.content
+
+    await ctx.guild.create_voice_channel(tournament_name)
+
+    await ctx.move_member(msg.author, tournament_name)
 
 if __name__ == "__main__":
     bot.run(TOKEN)
